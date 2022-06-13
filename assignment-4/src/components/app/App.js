e6cef51094f08c5ad68d79b57d916c6d9b3ec13e
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-06-13 12:38:48 +0300

    Code formatting

--- assignment-4/src/components/app/App.js
+++ assignment-4/src/components/app/App.js
@@ -3,7 +3,7 @@ import MainForm from "../mainForm/MainForm";
 function App() {
   return (
     <div className="App">
-<MainForm />
+      <MainForm />
     </div>
   );
 }
--- assignment-4/src/components/tasks/Tasks.jsx
+++ assignment-4/src/components/tasks/Tasks.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Tasks = () => {
-    return (
-        <div className='task'>
-        <p> A single task here</p>
+  return (
+    <div className="task">
+      <p> A single task here</p>
 
+      <span>#Monday </span>
 
-        <span>#Monday </span>
-
-            <form className='form-buttons' action="">
-                <button className='btn edit'>Edit</button>
-                <button className='btn delete'>Delete</button>
-                <button className='btn tag'>Add / Edit tags</button>
-            </form>
-        </div>
-    );
-}
+      <form className="form-buttons" action="">
+        <button className="btn edit">Edit</button>
+        <button className="btn delete">Delete</button>
+        <button className="btn tag">Add / Edit tags</button>
+      </form>
+    </div>
+  );
+};
 
 export default Tasks;
