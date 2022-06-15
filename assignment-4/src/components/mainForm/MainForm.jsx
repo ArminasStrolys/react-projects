fad348816becd2f1efabafa03d07fd447fa67afb
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-06-15 21:24:44 +0300

    Express install for further testing

--- assignment-4/src/components/mainForm/MainForm.jsx
+++ assignment-4/src/components/mainForm/MainForm.jsx
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import Tasks from '../tasks/Tasks';
-import Data from "../data/data.JSON"
+import Data from "../data/data.json"
+
+
 
 const MainForm = () => {
 
     const [toDoData, setToDoData] = useState([])
 
+    const tester = () => {
+        Data.tasks.push(10)
+        console.log(Data)
+    }
+
+
     useEffect(() => {
-        fetch(Data)
+        fetch("http://localhost:3001/tasks")
           .then((res) => res.json())
           .then((data) => setToDoData(data))
           .catch((error) => console.log(error));
       }, []);
-console.log(toDoData)
+
+      console.log(Data)
+      
     return (
+
+
+        
 <>
     <form className='main-form' action="">
         <input className='task-input' type="text" placeholder='Enter task' required/>
@@ -21,9 +34,9 @@ console.log(toDoData)
         <button className='btn-add'>Add task</button>
     </form>
     <h1 className='task-collection-title'>Task collection</h1>
-    {/* <Tasks />
-    <Tasks />
-    <Tasks /> */}
+    <form onSubmit={(e) => e.preventDefault()} action="">
+        <button onClick={tester}>CLICK ME</button>
+    </form>
     {toDoData.map((data)=><Tasks
         key={data.id}
         title={data.title}
