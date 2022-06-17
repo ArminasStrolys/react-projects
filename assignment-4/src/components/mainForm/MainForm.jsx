f4c9abcb87b517c11791d039b75ddb49e1279d08
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-06-17 11:55:44 +0300

    Optimized DELETE method

--- assignment-4/src/components/mainForm/MainForm.jsx
+++ assignment-4/src/components/mainForm/MainForm.jsx
@@ -1,97 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import Tasks from '../tasks/Tasks';
+import React, { useState, useEffect } from "react";
+import Tasks from "../tasks/Tasks";
 // this row is responsible for live update
-import Data from "../data/data.json"
+import Data from "../data/data.json";
 
 const MainForm = () => {
-
-const randomId = (min, max) => {
+  const randomId = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
+  };
 
-    const [toDoData, setToDoData] = useState([])
-    const [addTask, setAddTask] = useState({
-        id: 0,
-        title: '',
-        tags: ''
-    })
-    const getIdFromChild = (data) => {
-        deleteEntry(data)
-    }
+  const [toDoData, setToDoData] = useState([]);
+  const [addTask, setAddTask] = useState({
+    id: 0,
+    title: "",
+    tags: "",
+  });
+  const getIdFromChild = (data) => {
+    deleteEntry(data);
+  };
 
-    const handleTasks = (e, data) => {
-        e.preventDefault()
-        const copyTask = { ...addTask }
-        copyTask[data] = e.target.value;
-        setAddTask(copyTask)
-        createEntry()
-    }
+  const handleTasks = (e, data) => {
+    e.preventDefault();
+    const copyTask = { ...addTask };
+    copyTask[data] = e.target.value;
+    setAddTask(copyTask);
+    createEntry();
+  };
 
-// Fetches data from json file
-    useEffect(() => {
-        fetch("http://localhost:3001/tasks")
-          .then((res) => res.json())
-          .then((data) => setToDoData(data))
-          .catch((error) => console.log(error));
-      }, []);
+  // Fetches data from json file
+  useEffect(() => {
+    fetch("http://localhost:3001/tasks")
+      .then((res) => res.json())
+      .then((data) => setToDoData(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-// Creates entry in json file
-const createEntry = () => {
+  // Creates entry in json file
+  const createEntry = () => {
     fetch("http://localhost:3001/tasks", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(addTask)
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(addTask),
     }).then(
-        setAddTask({
-            id: addTask.id,
-            title: addTask.title,
-            tags: addTask.tags
+      setAddTask({
+        id: addTask.id,
+        title: addTask.title,
+        tags: addTask.tags,
       })
     );
-  }
+  };
 
-const deleteEntry = (data) => {
+  const deleteEntry = (data) => {
     fetch("http://localhost:3001/tasks/" + data, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(addTask)
-      }).then(
-        setAddTask({
-            id: "",
-            title: "",
-            tags: ""
-      })
-        );
-}
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+  };
 
+  return (
+    <>
+      <form onSubmit={handleTasks} className="main-form" action="">
+        <input
+          className="task-input"
+          type="text"
+          placeholder="Enter task"
+          onChange={(e) =>
+            setAddTask({
+              ...addTask,
+              title: e.target.value,
+              id: randomId(0, 999999999),
+            })
+          }
+          required
+        />
+        <input
+          className="tag-input"
+          type="text"
+          placeholder="Optional: add tags"
+          onChange={(e) => setAddTask({ ...addTask, tags: e.target.value })}
+        />
+        <button type="submit" className="btn-add">
+          Add task
+        </button>
+      </form>
+      <h1 className="task-collection-title">Task collection</h1>
 
-  
-    return (
-
-<>
-    <form onSubmit={handleTasks} className='main-form' action="">
-        <input className='task-input' type="text" placeholder='Enter task' onChange={(e) => setAddTask({ ...addTask, title: e.target.value, id:randomId(0,999999999) })} required/>
-        <input className='tag-input' type="text" placeholder='Optional: add tags' onChange={(e) => setAddTask({ ...addTask, tags: e.target.value })} />
-        <button type='submit' className='btn-add'>Add task</button>
-    </form>
-    <h1 className='task-collection-title'>Task collection</h1>
-
-    {toDoData.map((data, index)=><Tasks
-        key={data.id}
-        id={data.id}
-        title={data.title}
-        tags={data.tags}
-        index={index}
-        data={getIdFromChild}
-    />
-    )}
-</>
-    );
-
-}
+      {toDoData.map((data, index) => (
+        <Tasks
+          key={data.id}
+          id={data.id}
+          title={data.title}
+          tags={data.tags}
+          index={index}
+          data={getIdFromChild}
+        />
+      ))}
+    </>
+  );
+};
 
 export default MainForm;
