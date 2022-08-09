b1cb04dfd2fe353d0840567c130b0722bfc7ae33
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-08-09 17:00:58 +0300

    Some more layouts

---
+++ eyebrow-portfolio-site/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      Home site
+    </div>
+  );
+}
+
+export default Home;
--- eyebrow-portfolio-site/src/components/navi/Navi.jsx
+++ eyebrow-portfolio-site/src/components/navi/Navi.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Contacts from '../contacts/Contacts';
+import PricesAndServices from '../pricesAndServices/PricesAndServices'
+import Home from '../home/Home';
+import About from '../about/About';
 
 const Navi = () => {
   return (
     <div>
-      Navigation
+      Navigation:
+      <b>
+      <Home />
+      <PricesAndServices />
+      <Contacts />
+      <About />
+      </b>
     </div>
   );
 }
