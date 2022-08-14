977315a3fbfad6186a2ee8b6b75518489918259c
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-08-14 21:12:05 +0300

    Testing out another carousel

--- eyebrow-portfolio-site/src/components/home/Home.jsx
+++ eyebrow-portfolio-site/src/components/home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import eyes1 from '../../images/eyebrows1.jpg';
-import correction from '../../images/correction.webp';
-import henna from '../../images/henna.jpg';
+import carousel1 from '../../images/eyes-wide1.jpg';
 import { Carousel } from 'react-responsive-carousel';
 
 const Home = () => {
   return (
     <div className="home">
+      <h2>Pagrindinis</h2>
+
       <h2>Pagrindinis</h2>
       <div>Bendra info</div>
       <div className="carousel-section">
@@ -18,24 +18,20 @@ const Home = () => {
           showStatus="false"
           showIndicators="false"
           showThumbs="false"
-          showArrows="false"
-          // width="50%"
+          showArrows="true"
           centerMode="true"
           transitionTime="700"
           centerSlidePercentage="70"
           dynamicHeight="true"
         >
           <div>
-            <img src={eyes1} alt="eyes" />
-            {/* <p className="legend">Legend 1</p> */}
+            <img src={carousel1} alt="eyes" />
           </div>
           <div>
-            <img src={correction} alt="eyes" />
-            {/* <p className="legend">Legend 2</p> */}
+            <img src={carousel1} alt="eyes" />
           </div>
           <div>
-            <img src={henna} alt="eyes" />
-            {/* <p className="legend">Legend 3</p> */}
+            <img src={carousel1} alt="eyes" />
           </div>
         </Carousel>
       </div>
