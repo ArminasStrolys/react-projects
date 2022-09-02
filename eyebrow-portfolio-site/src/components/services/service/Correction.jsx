3555bc5cffebebdc0fcb0ad853f78eae91bb446c
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-02 17:08:46 +0300

    Update Correction.jsx

--- eyebrow-portfolio-site/src/components/services/service/Correction.jsx
+++ eyebrow-portfolio-site/src/components/services/service/Correction.jsx
@@ -10,8 +10,9 @@ export default function Correction() {
         Procedūra atliekama tiek vyrams, tiek moterims. Antakių korekcijos metu
         suteikiama teisinga, veidui tinkama antakių forma, pašalinant
         nereikalingus plaukelius bei išgaunant švarų, nepriekaištingą antakio
-        linkį. Procedūros metu naudojamas vaškas/pincetas. Atlikus korekciją
-        naudojamas raminamasis kremas, skirtas nuraminti paraudusią odą.
+        linkį. Procedūros metu yra apjungiamos dvi skirtingos technikos vaškas
+        ir pincetas. Atlikus korekciją naudojamas raminamasis kremas, skirtas
+        nuraminti paraudusią odą.
       </p>
     </article>
   );
