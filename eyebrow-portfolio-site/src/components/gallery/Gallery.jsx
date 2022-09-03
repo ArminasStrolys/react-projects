3f2b8a0e0ea33a41079822c73036e7a0f0c34158
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-03 18:00:26 +0300

    Added some gallery items

--- eyebrow-portfolio-site/src/components/gallery/Gallery.jsx
+++ eyebrow-portfolio-site/src/components/gallery/Gallery.jsx
@@ -4,11 +4,12 @@ import eyes2 from '../../images/eyebrows2.jpg';
 import eyes3 from '../../images/eyebrows3.jpg';
 import eyes4 from '../../images/eyebrows4.jpg';
 import eyes5 from '../../images/eyebrows5.jpg';
+import eyes6 from '../../images/eyebrows6.jpg';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
 const Gallery = () => {
-  const images = [eyes1, eyes2, eyes3, eyes4, eyes5];
+  const images = [eyes1, eyes2, eyes3, eyes4, eyes5, eyes6];
 
   return (
     <div>
