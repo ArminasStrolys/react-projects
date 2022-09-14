6b258f2cd64a91cade90602dea763ba395c978a2
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-14 19:21:39 +0300

    Code cleanup

--- eyebrow-portfolio-site/src/components/services/Services.jsx
+++ eyebrow-portfolio-site/src/components/services/Services.jsx
@@ -110,63 +110,6 @@ const PricesAndServices = () => {
               </div>
             </div>
           </div>
-          {/* <table className="customTable">
-            <thead>
-              <tr>
-                <th>Procedūra</th>
-                <th>Kaina</th>
-                <th>Trukmė</th>
-                <th>Trumpas aprašymas</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td> Antakių korekcija</td>
-                <td>5€</td>
-                <td>15 - 30 min.</td>
-                <td>
-                  Karštu vašku, pincetu pašalinami iš formos išeinantys
-                  plaukeliai
-                </td>
-              </tr>
-
-              <tr>
-                <td>Antakių korekcija + dažymas</td>
-                <td>15€</td>
-                <td>1 val. 30 min.</td>
-                <td>
-                  Atliekama korekcija, piešiamas karkasas, parenkama antakių
-                  dažų spalva pagal klientės norus.
-                </td>
-              </tr>
-
-              <tr>
-                <td>Antakių laminavimas + korekcija</td>
-                <td>15€</td>
-                <td>1 val. 30 min.</td>
-                <td>
-                  Tepamos specialios priemonės, po kurios plaukeliai tampa
-                  paslankūs, lengvai formuojami bei atliekama korekcija.
-                </td>
-              </tr>
-              <tr>
-                <td>Antakių laminavimas + dažymas</td>
-                <td>20€</td>
-                <td>2 val.</td>
-                <td>
-                  Tepamos specialios priemonės, po kurios plaukeliai tampa
-                  paslankūs, lengvai formuojami, atliekama korekcija bei
-                  dažymas.
-                </td>
-              </tr>
-              <tr>
-                <td>Ūsiukų depiliacija</td>
-                <td>5€</td>
-                <td>15 - 30 min.</td>
-                <td>Atliekama ūsiukų depiliacija vašku ir pincetu.</td>
-              </tr>
-            </tbody>
-          </table> */}
         </div>
 
         <Correction />
